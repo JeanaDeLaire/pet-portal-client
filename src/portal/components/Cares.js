090e4c2f5f347f6262c99239e39dddb6fad12a79
JeanaDeLaire <jeanadelaire@gmail.com>
2019-02-01 20:54:00 -0500

    fix margin on cares

--- src/portal/components/Cares.js
+++ src/portal/components/Cares.js
@@ -14,7 +14,7 @@ const Cares = props => {
   return (
     <div className='inde-cares'>
       {/*<p><b>ID</b>{ props.data._id }</p>*/}
-      <p>{ props.data.type }: { props.data.details }</p>
+      <p className='inde-care-paragraph'>{ props.data.type }: { props.data.details }</p>
       <Button className='button' bsStyle="primary" onClick={ deleteThisCare } type="submit" value="Remove"><i className="fas fa-trash-alt"></i></Button>
     </div>
   )
